0c065c3fa2c30a8bc614a5470df4a0a788496331
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-07-09 12:02:08 +0200

    Enable make command

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,24 +4,23 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+import src.config as cfg
+from src.data.stock import Stock
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
+@click.argument('symbol', type=click.STRING)
+def main(symbol:str):
     """ Runs data processing scripts to turn raw data from (../raw) into
         cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info(f'Creating dataset for {symbol}')
+    stock = Stock(symbol, save=True)
+
 
 
 if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    logging.basicConfig(**cfg.LOGGING_CONFIG)
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
