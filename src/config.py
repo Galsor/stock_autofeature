e633cf9b6670e253be2728a8784786e77339cf3d
Antoine Meilliez <meilliez.antoine@gmail.com>
2021-07-09 12:01:27 +0200

    Add Folders, loggings and columns

--- src/config.py
+++ src/config.py
@@ -1,7 +1,11 @@
 import os
 import dotenv
+import logging
 
 PROJECT_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
+DATA_DIR = os.path.join(PROJECT_DIR, "data")
+RAW_DATA_DIR = os.path.join(DATA_DIR, "raw") 
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
 
 DOTENV_PATH = os.path.join(PROJECT_DIR, '.env')
 dotenv.load_dotenv(DOTENV_PATH)
@@ -11,12 +15,18 @@ BREAKEVEN = 0.003
 
 #Features generator parameters
 SCALING_WINDOW = 10
+SMA_DEFAULT_WINDOW = 3
+
+# CONFIG VARIABLES - This values are filled dynamically by the pipeline
 CURRENT_COLS = None # To be filled after each step of the transformation
 FINTA_COLS = None # To Be filled when finta_transformer class instance is fitted
 SCALERS_NAMES = None # To Be filled when scalers.py is loaded
 SCALED_COLS = None
 DISTANCES_NAMES = None # To Be filled when distances.py is loaded
 DISTANCED_COLS = None
+NAN_OFFSET = None
+SELECTED_NAMES = None # Filled by Selectors
+SELECTED_COLS = None
 PASSTHROUGH_NAME = "passthrough"
 
 SORT_STOCK_ASCENDING = False
@@ -82,3 +92,10 @@ META_SYMBOL = '2. Symbol'
 META_LAST_REFRESH = '3. Last Refreshed'
 META_OUTPUT_SIZE = '4. Output Size'
 META_TIME_ZONE = '5. Time Zone'
+
+
+
+LOGGING_CONFIG ={
+    "format": '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    "level": logging.INFO
+}
